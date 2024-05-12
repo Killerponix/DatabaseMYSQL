2685e8fc222fee3c16507b37443333fd9e29d7e3
Killerponix <100039163+Killerponix@users.noreply.github.com>
2024-05-12 10:47:38 +0200

    MYSQL Update

--- src/main/java/app/databasemysql/MySQL.java
+++ src/main/java/app/databasemysql/MySQL.java
@@ -1,5 +1,113 @@
-package app.databasemysql;
 
+package app.databasemysql;
+import java.sql.*;
+import java.sql.DriverManager;
+//import java.sql.PreparedStatement;
+import java.sql.SQLException;
+public class MySQL {
+    private static final String host = "SimonPC";
+    private static final String port = "3306";
+    private static final String database = "belegschaft";
+    private static final String username = "Simon";
+    private static final String password = "CVH";
+
+    private static Connection con;
+
+    public static boolean isConnected() {
+        return (con == null ? false : true);
+    }
+    public static void connect() throws ClassNotFoundException {
+        if(!isConnected()) {
+            try {
+
+                Class.forName("com.mysql.cj.jdbc.Driver");
+
+                con = DriverManager.getConnection("jdbc:mysql://" + host + ":"+port+"/"+database, username, password);
+                System.out.println("[MySQL] Verbunden");
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+    public static void disconnect() {
+        if(isConnected()) {
+            try {
+                con.close();
+                System.out.println("[MySQL] Verbindung Geschlossen");
+            } catch (SQLException e) {
+                System.out.println("rein");
+                e.printStackTrace();
+
+
+            }
+        }
+    }
+    //	public static void update(String qry) {
+//		try {
+//			Statement ps = con.createStatement();
+//			ResultSet res = ps.executeQuery(qry);
+//            int ang_nr;
+//            int salary;
+//            Date d1,d2;
+//            System.out.println("AngestelltenNR. " + "Gehalt" + "Von" +"Bis");
+//            while (res.next()) {
+//                ang_nr = res.getInt(1);
+//                salary = res.getInt(2);
+//            	d1 = res.getDate(3);
+//                d2 = res.getDate(4);
+//                boolean exist = true;
+//                System.out.println(ang_nr + " " + salary + " " + d1 + " " + d2);
+//            };
+//        ps.close();
+//        res.close();
+//
+//		} catch (SQLException e) {
+//			e.printStackTrace();
+//
+//		}
+//	}
+    public static void executeQuery(String query) {
+        try {
+            Statement statement = con.createStatement();
+            ResultSet resultSet = statement.executeQuery(query);
+
+            ResultSetMetaData metaData = resultSet.getMetaData();
+            int columnCount = metaData.getColumnCount();
+
+            for (int i = 1; i <= columnCount; i++) {
+                System.out.printf("%-10s   ", metaData.getColumnName(i));
+            }
+            System.out.println("");
+
+            while (resultSet.next()) {
+                for (int i = 1; i <= columnCount; i++) {
+                    System.out.printf("%-10s   ", resultSet.getString(i));
+                }
+                System.out.println("");
+            }
+
+            statement.close();
+            resultSet.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    public static void executeUpdate(String query) {
+        try {
+            Statement statement = con.createStatement();
+            int rowsAffected = statement.executeUpdate(query);
+            System.out.println("Rows affected: " + rowsAffected);
+            statement.close();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
+
+//old Code
+
+/*
 import java.sql.*;
 import java.sql.DriverManager;
 import java.sql.PreparedStatement;
@@ -125,3 +233,4 @@ public class MySQL {
 
 }
 
+*/
